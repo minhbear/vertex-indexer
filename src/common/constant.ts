c9037152832004db2692e6818de9b636357a0239
minhbear <minhbeardev@gmail.com>
2025-05-13 04:17:02 +0700

    WIP: handle billing system

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,3 +1,5 @@
+import { PublicKey } from '@solana/web3.js';
+
 export const TypeColumn = [
   'varbinary',
   'varchar',
@@ -9,3 +11,20 @@ export const TypeColumn = [
   'date',
   'bigint',
 ];
+
+export const TIME_WAIT_RESEND_TX = 2_000;
+export const DELAY_RETRY_TX = 0;
+export const DEFAULT_RETRIES = 3;
+export const TIME_WAIT_RETRY_PARSE_TX = 1000;
+export const DEFAULT_MICRO_LAMPORTS = 50000;
+export const DEFAULT_COMPUTE_UNIT_LIMIT = 1000000;
+export const DEFAULT_TIMEOUT_SEND_TX = 90_000;
+export const MAX_COMPUTE_UNIT = 1_400_000;
+export const PRIORITY_FEE_MICRO_LAMPORT = 100_000;
+export const LIQUIDITY_COMPUTE_UNIT_LIMIT = 400_000;
+export const DEFAULT_TX_HASHES = [
+  '1111111111111111111111111111111111111111111111111111111111111111',
+];
+export const DEFAULT_PUBLICK_KEY = new PublicKey(
+  '11111111111111111111111111111111',
+);
